23883d3f417756e0fae2425855d49fa17d77d37b
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2020-03-16 21:33:15 +0100

    init

---
+++ CustomSaveArt/CustomSaveArt.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection;
+using Modding;
+using UnityEngine;
+using ModCommon.Util;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomSaveArt
+{
+    public class CustomSaveArt : Mod
+    {
+        internal static CustomSaveArt Instance;
+
+        public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+            Instance = this;
+
+            On.UnityEngine.UI.SaveSlotButton.PresentSaveSlot += OnSaveSlotButtonPresentSaveSlot;
+
+            //GameManager.instance.StartCoroutine(AddCustomAreaBackground());
+            Log("Initialized");
+        }
+
+        private void OnSaveSlotButtonPresentSaveSlot(On.UnityEngine.UI.SaveSlotButton.orig_PresentSaveSlot orig, UnityEngine.UI.SaveSlotButton self, SaveStats saveStats)
+        {
+            Log("!OnSaveSlotButtonPresentSaveSlot");
+
+            SaveSlotBackgrounds ssbg = self.saveSlots;
+            Sprite cabgSprite = null;
+
+            #region Load the Custom Area Background Sprite
+            Assembly _asm = Assembly.GetExecutingAssembly();
+            using (Stream s = _asm.GetManifestResourceStream("CustomSaveArt.Resources.CustomAreaArt.png"))
+            {
+                if (s != null)
+                {
+                    byte[] buffer = new byte[s.Length];
+                    s.Read(buffer, 0, buffer.Length);
+                    s.Dispose();
+
+                    //Create texture from bytes
+                    var tex = new Texture2D(2, 2);
+
+                    tex.LoadImage(buffer, true);
+
+                    // Create sprite from texture
+                    cabgSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+                }
+            }
+            #endregion
+            if (cabgSprite != null)
+            {
+                if ((ssbg.areaBackgrounds[0].areaName != GlobalEnums.MapZone.OUTSKIRTS) || !(ssbg.areaBackgrounds[0].backgroundImage.Equals(cabgSprite)))
+                {
+                    AreaBackground[] customAreaBackgrounds = new AreaBackground[ssbg.areaBackgrounds.Length + 1];
+                    ssbg.areaBackgrounds.CopyTo(customAreaBackgrounds, 1);
+                    customAreaBackgrounds[0] = new AreaBackground();
+                    customAreaBackgrounds[0].areaName = GlobalEnums.MapZone.OUTSKIRTS;
+                    customAreaBackgrounds[0].backgroundImage = cabgSprite;
+                    ssbg.areaBackgrounds = customAreaBackgrounds;
+                }
+            }
+            Log("~OnSaveSlotButtonPresentSaveSlot");
+
+            orig(self, saveStats);
+        }
+    }
+}
---
+++ CustomSaveArt/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("CustomSaveArt")]
+[assembly: AssemblyDescription("A Hollow Knight Mod")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomSaveArt")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("357e65c5-6d19-4459-a58f-311db901cf83")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
